99f8796f037576e8c8c1b7b524743459fd0cb451
dida <dida.ratsimba@gmail.com>
2016-11-22 19:04:13 +0300

    some attributes to do some stuff next time

---
+++ src/main/java/mg/rojo/Company.java
@@ -0,0 +1,11 @@
+package mg.rojo;
+
+/**
+ * Created by dida on 11/22/16.
+ */
+public class Company {
+
+    private String brand;
+    private Person owner;
+    private Integer yearOfCreation;
+}
--- src/main/java/mg/rojo/Person.java
+++ src/main/java/mg/rojo/Person.java
@@ -1,7 +1,15 @@
 package mg.rojo;
 
+import java.util.Calendar;
+
 /**
  * Created by dida on 11/22/16.
  */
 public class Person {
+
+    private String firstName;
+    private String lastName;
+    private Boolean gender;
+    private Calendar dateOfBirth;
+    private Company company;
 }
